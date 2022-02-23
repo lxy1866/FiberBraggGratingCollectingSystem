76bb10c8b2a3444cba1f668d7dd44d5706e591e2
lxy1866 <xiaoyanlin1866@gmail.com>
2022-02-23 20:29:54 +0800

    制作the-footer组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,18 +2,18 @@
   <a-layout>
     <the-header></the-header>
     <router-view/>
-    <a-layout-footer style="text-align: center">
-      FiberBraggGrating Collecting System ©2022 Created by Danly
-    </a-layout-footer>
+    <the-footer></the-footer>
   </a-layout>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
 import TheHeader from '@/components/the-header.vue';
+import TheFooter from '@/components/the-footer.vue';
 export default defineComponent({
   name: 'app',
   components: {
     TheHeader,
+    TheFooter
   },
 });
 </script>
---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    FiberBraggGrating Collecting System ©2022 Created by Danly
+  </a-layout-footer>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer',
+});
+</script>
