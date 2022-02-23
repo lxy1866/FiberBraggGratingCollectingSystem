c3c395f084b1d5f60287ab4c19cbb1d4ddd60ad2
lxy1866 <xiaoyanlin1866@gmail.com>
2022-02-23 14:58:31 +0800

    修改启动文案

--- src/main/java/top/kaluna/modbusTcp/ModbusTcpApplication.java
+++ src/main/java/top/kaluna/modbusTcp/ModbusTcpApplication.java
@@ -1,17 +1,25 @@
 package top.kaluna.modbusTcp;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @MapperScan("top.kaluna.modbusTcp.mapper")
 @EnableRabbit
 public class ModbusTcpApplication {
 
-	public static void main(String[] args) throws InterruptedException {
-		SpringApplication.run(ModbusTcpApplication.class, args);
+	private static final Logger LOG = LoggerFactory.getLogger(ModbusTcpApplication.class);
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(ModbusTcpApplication.class);
+		Environment environment = app.run(args).getEnvironment();
+		LOG.info("启动成功！！");
+		LOG.info("地址：\thttp://127.0.0.1:{}",environment.getProperty("server.port"));
+
 		//new MasterExample(1, 1).start();
 	}
 
