92f582d65e27bbe5907d8094b822c25d94423148
lxy1866 <xiaoyanlin1866@gmail.com>
2022-02-25 21:35:18 +0800

    用户表设计，并生成持久层代码

--- doc/all.sql
+++ doc/all.sql
@@ -16,3 +16,13 @@ INSERT INTO physical_value (val2, val1, val3, val4, val5, val6, id) VALUES (267.
 INSERT INTO physical_value (val2, val1, val3, val4, val5, val6, id, create_time) VALUES (267.7500, 61.5043, 118.6680, 232.4906, 94.8208, 274.2150, 3, '2017-10-27 12:15:42.664');
 INSERT INTO physical_value (val2, val1, val3, val4, val5, val6, id, create_time) VALUES (267.7500, 61.5043, 118.6680, 232.4906, 94.8208, 274.2150, 4, '2022-2-23 12:15:42.664');
 INSERT INTO physical_value (val2, val1, val3, val4, val5, val6, id, create_time) VALUES (267.7500, 61.5043, 118.6680, 232.4906, 94.8208, 274.2150, 5, '2017-2-23 12:15:42.664');
+
+drop table if exists `user`;
+create table `user`(
+                       `id` bigint not null comment 'ID',
+                       `login_name` varchar(50) not null comment '登陆名',
+                       `name` varchar(50) comment '昵称',
+                       `password` char(32) not null comment '密码',
+                       primary key (`id`),
+                       unique key `login_name_unique` (`login_name`)
+)engine = innodb default charset = utf8mb4 comment = '用户';
