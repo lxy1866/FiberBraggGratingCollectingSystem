6e12d1795fad14e7584ba4911158b0ae9254d7f8
lxy1866 <xiaoyanlin1866@gmail.com>
2022-02-25 10:41:49 +0800

    增加物质应变值管理页面 admin-physicalValue.vue页面开发，路由，菜单，并修改页面布局

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/home.vue'
 import About from '../views/about.vue'
+import AdminPhysicalValue from '../views/admin/admin-physicalValue.vue'
+
+import component from "*.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -16,7 +19,12 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     //component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
-  }
+  },
+  {
+    path: '/admin/physicalValue',
+    name: 'AdminPhysicalValue',
+    component: AdminPhysicalValue
+  },
 ]
 
 const router = createRouter({
---
+++ web/src/views/admin/admin-physicalValue.vue
@@ -0,0 +1,11 @@
+<template>
+  <a-layout>
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <div class="admin-ebook">
+        <h1>物理值管理</h1>
+      </div>
+    </a-layout-content>
+  </a-layout>
+</template>
