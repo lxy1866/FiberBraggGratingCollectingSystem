83c3ca4082b2f32f58229a67bac078520b7712b0
lxy1866 <xiaoyanlin1866@gmail.com>
2022-02-28 10:08:24 +0800

    增加路由登陆拦截，未登陆时跳到首页

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -3,9 +3,11 @@ import Home from '../views/home.vue'
 import About from '../views/about.vue'
 import AdminPhysicalValue from '../views/admin/admin-physicalValue.vue'
 import Login from '../views/login.vue'
-
+import {Tool} from "@/util/tool";
+import store from "@/store";
 import component from "*.vue";
 import AdminUser from '../views/admin/admin-user.vue'
+import login from "@/views/login.vue";
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
@@ -29,7 +31,10 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/admin/physicalValue',
     name: 'AdminPhysicalValue',
-    component: AdminPhysicalValue
+    component: AdminPhysicalValue,
+    meta:{
+      loginRequire: true
+    }
   },
   {
     path: '/admin/user',
@@ -45,5 +50,22 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
-
+//路由登录拦截
+router.beforeEach((to, from, next) =>{
+  // 要不要对meta.loginRequire属性做监控拦截
+  if(to.matched.some(function (item){
+    console.log(item, "是否需要登录校验：", item.meta.loginRequire);
+    return item.meta.loginRequire
+  })){
+    const loginUser = store.state.user;
+    if(Tool.isEmpty(loginUser)){
+      console.log("用户未登录！");
+      next('/');
+    }else {
+      next();
+    }
+  }else {
+    next();
+  }
+})
 export default router
