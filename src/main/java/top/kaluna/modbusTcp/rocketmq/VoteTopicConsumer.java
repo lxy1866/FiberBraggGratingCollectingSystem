41c510a6344487cf52c7c70d6512da763c57344e
lxy1866 <xiaoyanlin1866@gmail.com>
2022-02-28 14:11:47 +0800

    使用RocketMQ解耦点赞通知功能，例如在需要的service添加 rocketMQTemplate.convertAndSend("VOTE_TOPIC","《"+docDb.getName()+"》被点赞");

---
+++ src/main/java/top/kaluna/modbusTcp/rocketmq/VoteTopicConsumer.java
@@ -0,0 +1,34 @@
+package top.kaluna.modbusTcp.rocketmq;
+
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import top.kaluna.modbusTcp.websocket.WebSocketServer;
+
+
+import javax.annotation.Resource;
+
+
+/**
+ * @author Yuery
+ * @date 2022/2/28/0028 - 14:07
+ */
+@Service
+@RocketMQMessageListener(consumerGroup = "default",topic = "VOTE_TOPIC")
+public class VoteTopicConsumer implements RocketMQListener<MessageExt> {
+
+    private static final Logger LOG =  LoggerFactory.getLogger(VoteTopicConsumer.class);
+
+    @Resource
+    public WebSocketServer webSocketServer;
+    @Override
+    public void onMessage(MessageExt messageExt){
+        byte[] body = messageExt.getBody();
+        LOG.info("ROCKETMQ收到消息：{}", new String(body));
+        webSocketServer.sendInfo(new String(body));
+    }
+}
+
