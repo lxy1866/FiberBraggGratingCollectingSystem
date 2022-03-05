6318bc036469c620dccb198b0ebf874dec715e50
lxy1866 <xiaoyanlin1866@gmail.com>
2022-03-05 19:59:15 +0800

    完成扫描physical_value这张表并插入breakpoint_record表的定时任务

--- src/main/java/top/kaluna/modbusTcp/job/PhysicalValueJob.java
+++ src/main/java/top/kaluna/modbusTcp/job/PhysicalValueJob.java
@@ -5,8 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import top.kaluna.modbusTcp.service.BreakpointRecordService;
 import top.kaluna.modbusTcp.util.SnowFlake;
-
+import org.springframework.transaction.annotation.Transactional;
 import javax.annotation.Resource;
 
 /**
@@ -18,17 +19,23 @@ public class PhysicalValueJob {
     private final Logger LOG = LoggerFactory.getLogger(PhysicalValueJob.class);
     @Resource
     private SnowFlake snowFlake;
+
+    @Resource
+    private BreakpointRecordService breakpointRecordService;
     /**
-     * 每30秒更新电子书信息
-     *  从5秒开始,每30秒执行一次
+     * 每30分钟更新断点记录表
+     * 从0秒开始,每30分钟执行一次
+     * 从physical_value表查询前半个小时tag！=0的记录到breakpoint_record记录表中
      */
-    @Scheduled(cron = "5/30 * * * * ?")
+    @Transactional(rollbackFor = Exception.class)
+    //@Scheduled(cron = "0 0/30 * * * ?" )
+    @Scheduled(cron = "0/5 * * * * ? ")
     public void cron(){
         //增加日志流水号
         MDC.put("LOG_ID",String.valueOf(snowFlake.nextId()));
-        LOG.info("更新电子书下的文档数据开始");
+        LOG.info("更新断点记录表开始");
         long start = System.currentTimeMillis();
-        //docService.updateEbookInfo();
-        LOG.info("更新电子书下的文档数据结束，耗时：{}毫秒",System.currentTimeMillis() - start);
+        breakpointRecordService.insertBreakpointInfoByScanPhysicalValue();
+        LOG.info("更新断点记录表结束，耗时：{}毫秒",System.currentTimeMillis() - start);
     }
 }
