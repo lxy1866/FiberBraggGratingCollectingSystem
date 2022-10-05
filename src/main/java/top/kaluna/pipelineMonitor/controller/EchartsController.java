72e830736fecefd7c1463e080925031dea15f97a
ycy <1191364476@qq.com>
2022-10-05 13:40:45 +0800

    2022.10.5

---
+++ src/main/java/top/kaluna/pipelineMonitor/controller/EchartsController.java
@@ -0,0 +1,75 @@
+package top.kaluna.pipelineMonitor.controller;
+
+import jxl.read.biff.BiffException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import jxl.Workbook; //java读取excel表使用的类
+import jxl.Cell;  //java读取表格里的单元格的类
+import jxl.Sheet;
+import top.kaluna.pipelineMonitor.resp.CommonResp;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/echarts")
+public class EchartsController {
+
+    @GetMapping("/leftTopX")
+    public CommonResp<List<String>> getX() throws BiffException, IOException {
+        CommonResp<List<String>> resp = new CommonResp<>();
+        List<String> list = readxlsStr("F:\\数据\\6月18日至7月27日-12%变形点施工期间管道位移监测数据-显示B列.xls",0);
+        resp.setContent(list);
+        return resp;
+    }
+
+    @GetMapping("/leftTopY")
+    public CommonResp<List<Double>> getY() throws BiffException, IOException {
+        CommonResp<List<Double>> resp = new CommonResp<>();
+        List<Double> list = readxlsDou("F:\\数据\\6月18日至7月27日-12%变形点施工期间管道位移监测数据-显示B列.xls",1);
+        resp.setContent(list);
+        return resp;
+    }
+
+    public static ArrayList<String> readxlsStr(String filepath, int index) throws IOException, BiffException {
+        ArrayList<String> colList = new ArrayList<>();
+        String value;
+        File file = new File(filepath);
+        InputStream inputStream = new FileInputStream(file.getAbsoluteFile());
+        Workbook workbook = Workbook.getWorkbook(inputStream);
+        Sheet sheet = workbook.getSheet(0);
+        int rows = sheet.getRows();
+        for (int i = 2;i < rows;i++){
+            Cell cell = sheet.getCell(index,i);
+            value = cell.getContents();
+            colList.add(value);
+        }
+        return colList;
+    }
+
+    public static ArrayList<Double> readxlsDou(String filepath, int index) throws IOException, BiffException {
+        ArrayList<Double> colList = new ArrayList<>();
+        Double value;
+        File file = new File(filepath);
+        InputStream inputStream = new FileInputStream(file.getAbsoluteFile());
+        Workbook workbook = Workbook.getWorkbook(inputStream);
+        Sheet sheet = workbook.getSheet(0);
+        int rows = sheet.getRows();
+        for (int i = 2;i < rows;i++){
+            Cell cell = sheet.getCell(index,i);
+            if(cell.getContents().contains("﹢")){
+                value = Double.valueOf(cell.getContents().replace("﹢",""));
+            }else{
+                value = Double.valueOf(cell.getContents());
+            }
+            colList.add(value);
+        }
+        return colList;
+    }
+}
+
--- web/src/components/leftTopChart.vue
+++ web/src/components/leftTopChart.vue
@@ -7,282 +7,51 @@ import * as echarts from 'echarts';
 import {defineComponent, onMounted} from "vue";
 import axios from "axios";
 import {message} from "ant-design-vue";
-//多少天 多少个传感器 第一天所有的传感器都赋值
-interface avgSensor {
-  id:number,
-  arraySn:number,
-  sensorNodeName: string,
-  avg: number,
+interface Xvalue {
   date: string
 }
-const myDate = new Date();
-let year = myDate.getFullYear(); //获取完整的年份(4位,1970-???)
-let lastMonth = myDate.getMonth();
-if(lastMonth == 0){
-  lastMonth=12;
-  year=year-1;
+interface Yvalue {
+  date: number
 }
-
-function getLastMonthTotalDay(){
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = date.getMonth();
-  const d = new Date(year, month, 0);
-  return d.getDate();
+function handleQueryAvgX(){
+  return axios.get("/echarts/leftTopX")
 }
-function getLastWeekFirstDay(){
-  // 获取当前时间
-  let currentDate = new Date()
-  // 返回date是一周中的某一天
-  let week = currentDate.getDay()
-  // 返回date是一个月中的某一天
-  // let month = currentDate.getDate()
-  // 一天的毫秒数
-  let millisecond = 1000 * 60 * 60 * 24
-  // 减去的天数
-  let minusDay = week !== 0 ? week - 1 : 6
-  // 获得当前周的第一天
-  let currentWeekDayOne = new Date(currentDate.getTime() - (millisecond * minusDay))
-  // 上周最后一天即本周开始的前一天
-  let priorWeekLastDay = new Date(currentWeekDayOne.getTime() - millisecond)
-  // 上周的第一天
-  let priorWeekFirstDay = new Date(priorWeekLastDay.getTime() - (millisecond * 6))
-  return priorWeekFirstDay;
-}
-//原来是统计一个月
-//let day = getLastMonthTotalDay() // 31
-
-//const title = myDate.getFullYear() + '年' + (myDate.getMonth()) + '月管道位移数据';
-const title = myDate.getFullYear() + '年' + (myDate.getMonth()) + '月管道位移数据';
-function handleQueryLeftTopAttribute() {
-  return axios.get("/home/leftTopAttributeGet")
-}
-function handleQueryAvg(){
-  return axios.get("/home/leftTopDataGet")
+function handleQueryAvgY(){
+  return axios.get("/echarts/leftTopY")
 }
 export default defineComponent({
   name: 'leftTopChart',
-  setup() {
-    onMounted( async ()=>{
-      let leftTopAttributeContent = {
-        eachArrayNum:'',
-        arrayTotal:2
-      };
-      let leftTopDataGet;
-      await handleQueryLeftTopAttribute().then(res => {
-        leftTopAttributeContent = res.data.content
-      })
-      let array: avgSensor[][];
-      array = await handleQueryAvg().then(res =>{
-        leftTopDataGet = res.data.content
-        console.log("res.data.content", leftTopDataGet)
-        if(!res.data.success){
-          message.info("左图后端数据被污染，请联系管理员")
-        }
-        return leftTopDataGet;
-      })
-      let day = 14
-      if(array.length == getLastMonthTotalDay()*2){
-        day = getLastMonthTotalDay() *2;
-      }
-      let sensorNum1 = Number(leftTopAttributeContent.eachArrayNum.split("_")[0])
-      let sensorNum2 = Number(leftTopAttributeContent.eachArrayNum.split("_")[1])
-      let  sensorNum = sensorNum1 + sensorNum2
-      let data :number[][] = [];
-      for(let i = 0; i < day; i++){
-        data[i] = [];
-        for(let j = 0; j < sensorNum; j++){
-          data[i][j] = Number((array[i][j].avg).toFixed(2))
-        }
-      }
+  setup(){
+    onMounted(async ()=>{
       const chartDom = document.getElementById('leftTopChart')!;
       const myChart = echarts.init(chartDom);
-      const option={
-        baseOption: {
-          timeline: {
-            axisType: 'category',
-            // realtime: false,
-            // loop: false,
-            autoPlay: true,
-            // currentIndex: 2,
-            playInterval: 1000,
-            // controlStyle: {
-            //     position: 'left'
-            // },
-            data: [''],
-          },
-          toolbox: {
-            show: true,
-            feature: {
-              dataView: {readOnly: false},
-              restore: {},
-              magicType: { type: ['line', 'bar'] },
-              saveAsImage: {}
-            }
-          },
-          title: {
-            text: '管道位移数据(单位：mm)',
-            textStyle: {
-              color: '#ffffff',
-              fontFamily: '宋体',
-            },
-          },
-          calculable: true,
-          grid: {
-            top: 60,
-            left:100,
-            right:50,
-            bottom: 70,
-            tooltip: {
-              trigger: 'axis',
-              axisPointer: {
-                type: 'shadow',
-                label: {
-                  show: true
-                }
-              }
-            }
-          },
-          yAxis: [
-            {
-              name:'管道传感器编号',
-              type: 'category',
-              axisLabel: {interval: 0},
-              data: [''],
-              splitLine: {show: false}
-            }
-          ],
-          xAxis: [
-            {
-              type: 'value',
-              name: '位移值',
-              nameLocation:'end',
-            }
-          ],
-          visualMap:{
-            orient: 'vertical',
-            inRange: {
-              color: ['#ef2b2d','#82043c', '#442b1e', '#330a17','#70247d',
-                '#b22234','#ef7878', '#559069', '#a45060', '#670044','#565965',
-                '#340569', '#586042','#619405', '#945006','#586473', '#124995',
-                '#759594', '#c56600','#4899e7', '#678900', '#895697',]
-            },
-            min: 0,
-            max: 30,
-          },
-          series: [
-            {
-              name: '位移值',
-              type: 'bar',
-              label: {
-                show: true,
-                precision: 0,
-                position: 'right',
-                valueAnimation: true,
-                fontFamily: 'monospace',
-              }
-            },
-          ],
+      const option = {
+        xAxis: {
+          type: 'category',
+          data: []
         },
-        options:[{}]
-      }
-      let timelineData = [];
-      let optionsData = [];
-      //day应该按照后端的数据长度来设置
-      if(day == 14){
-        //day是上一周天数的2倍
-        for(let i = 0 ; i < day; i++){
-          //时间线
-          timelineData.push((i+1).toString())
-          if(i % 2 == 0){
-            //一周的话就插入7次
-            optionsData.push(
-                {
-                  title:{
-                    text: year + '年' + (((getLastWeekFirstDay().getDate()+(Math.floor(i/2))) <= getLastMonthTotalDay()) ? (getLastWeekFirstDay().getMonth()+1):(getLastWeekFirstDay().getMonth()+2))+ '月' +
-                        (((getLastWeekFirstDay().getDate()+(Math.floor(i/2)))<= getLastMonthTotalDay()) ? (getLastWeekFirstDay().getDate()+(Math.floor(i/2))):(getLastWeekFirstDay().getDate()+(Math.floor(i/2)) - getLastMonthTotalDay()))+'日'+
-                        ("星期"+"一二三四五六日".charAt(getLastWeekFirstDay().getDay()+(Math.floor(i/2))-1))+
-                        '上午22%变形点管道位移数据'
-                  },
-                  series: [
-                    {
-                      data: data[i],
-                    },
-                  ]
-                }
-            )
-          }else {
-            optionsData.push(
-                {
-                  title:{
-                    text: year + '年' + (((getLastWeekFirstDay().getDate()+(Math.floor(i/2))) <= getLastMonthTotalDay()) ? (getLastWeekFirstDay().getMonth()+1):(getLastWeekFirstDay().getMonth()+2))+ '月' +
-                        (((getLastWeekFirstDay().getDate()+(Math.floor(i/2)))<= getLastMonthTotalDay()) ? (getLastWeekFirstDay().getDate()+(Math.floor(i/2))):(getLastWeekFirstDay().getDate()+(Math.floor(i/2)) - getLastMonthTotalDay()))+'日'+
-                        ("星期"+"一二三四五六日".charAt(getLastWeekFirstDay().getDay()+(Math.floor(i/2))-1))+
-                        '下午22%变形点管道位移数据'
-                  },
-                  series: [
-                    {
-                      data: data[i],
-                    },
-                  ]
-                }
-            )
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            data: [],
+            type: 'line',
+            smooth: true
           }
-        }
-      }else {
-        //day是上个月的天数的2倍
-        for(let i = 0 ; i < day; i++){
-          //时间线
-          timelineData.push((i+1).toString())
-          if(i % 2 == 0){
-            //一周的话就插入7次
-            optionsData.push(
-                {
-                  title:{
-                    text: year + '年' + lastMonth + '月'+ (Math.floor(i/2)+1 )  +'日'+
-                        '上午22%变形点管道位移数据'
-                  },
-                  series: [
-                    {
-                      data: data[i],
-                    },
-                  ]
-                }
-            )
-          }else {
-            optionsData.push(
-                {
-                  title:{
-                    text: year + '年' + lastMonth + '月' + (Math.floor(i/2)+1)  +'日'+
-                        '下午22%变形点管道位移数据'
-                  },
-                  series: [
-                    {
-                      data: data[i],
-                    },
-                  ]
-                }
-            )
-          }
-        }
+        ]
       }
-
-      let yData = [];
-      for(let i = 1; i <= leftTopAttributeContent.arrayTotal; i++){
-        let nums = Number(leftTopAttributeContent.eachArrayNum.split("_")[i-1])
-        for(let j = 1; j <= nums; j++){
-          yData.push("阵列"+i+"编号"+j)
-        }
-      }
-      option.baseOption.timeline.data = timelineData;
-      option.baseOption.yAxis[0].data = yData
-      option.options = optionsData;
-      //@ts-ignore
+      handleQueryAvgX().then(res =>{
+        option.xAxis.data = res.data.content;
+        console.log(res.data.content);
+      })
+      handleQueryAvgY().then(res =>{
+        option.series[0].data = res.data.content;
+        console.log(res.data.content);
+      })
       myChart.setOption(option)
-    });
-    return {
-    }
-  },
+    })
+  }
 })
 </script>
 <style scoped>
